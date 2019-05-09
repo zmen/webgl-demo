acb8bc3c45b33dbfef65ec95963d097eda7d329a
HughHzWu <99571603@qq.com>
2019-05-09 13:36:46 +0800

    Updates

---
+++ precache-manifest.68a0b1afdb4e1606adf2f3d5a44f84ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6cb5acebf7c83768823647eabdf86c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a67dec8bcbead99aec1",
+    "url": "/static/css/2.36ffcbb4.chunk.css"
+  },
+  {
+    "revision": "6044d8bc1accfcddbc05",
+    "url": "/static/css/main.850409a0.chunk.css"
+  },
+  {
+    "revision": "6a67dec8bcbead99aec1",
+    "url": "/static/js/2.7e354833.chunk.js"
+  },
+  {
+    "revision": "6044d8bc1accfcddbc05",
+    "url": "/static/js/main.6ffd3a09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43b81679ace0406099cf3c892a25b9f2",
+    "url": "/static/media/TShirt.43b81679.png"
+  },
+  {
+    "revision": "77585d9f47376f9b3204f08ed32b64b2",
+    "url": "/static/media/test.77585d9f.png"
+  }
+]);

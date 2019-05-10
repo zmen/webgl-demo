c0388111d5bf9021525c7bc04d2a27e9e9b30356
gusenlin <senlin.gu@hand-china.com>
2019-05-10 13:33:38 +0800

    提交代码

--- src/routes/layout/index.js
+++ src/routes/layout/index.js
@@ -1,17 +1,40 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, {useContext, useEffect, useRef} from 'react';
 import CanvasContext from '../../context/CanvasContext';
+import './draw.css';
+import imgIcon from './images/template.jpeg'
+import drawHandler from './draw.js'
 
 export default function Layout() {
-  const canvasRef = useRef();
-  const { width, height } = useContext(CanvasContext);
+    const canvasRef = useRef();
+    const {width, height} = useContext(CanvasContext);
 
-  useEffect(() => {
-    const { current } = canvasRef;
-    if (current) {
+    useEffect(() => {
+        const {current} = canvasRef;
+
+        if (current) {
+            let ctx = current.getContext('2d');
+            console.log(ctx);
+            let img = new Image();
+            img.src = imgIcon;
+            img.onload = function () {
+                drawHandler.init(ctx, [img]);
+            }
+        }
+    }, [canvasRef, width, height]);
+
+    function change() {
+        drawHandler.draw();
     }
-  }, [canvasRef, width, height]);
 
-  return (
-    <canvas ref={canvasRef} width={width} height={height} />
-  );
+    return (
+        <div>
+            <div>
+                <button onClick={() => {
+                    change()
+                }}>转换
+                </button>
+            </div>
+            <canvas className='auto-layout' ref={canvasRef} width={width} height={height}/>
+        </div>
+    );
 }

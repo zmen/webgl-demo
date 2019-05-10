8d304b299586ac2b0572531a39d4b9bfe2647d5b
HughHzWu <99571603@qq.com>
2019-05-10 11:45:45 +0800

    [IMP] Glich demo

--- src/routes/glich/index.js
+++ src/routes/glich/index.js
@@ -1,244 +1,97 @@
-import React, { Component } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { Button } from 'antd';
-import './filter.css';
+import CanvasContext from '../../context/CanvasContext';
+import V3D12_GlichImpl from '../../v3d/V3D12_GlichImpl';
 import TestImg from './test.png';
 import Test1Img from './test1.png';
 import Test2Img from './test2.png';
+import './filter.css';
 
-class WrappedFilter extends Component {
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  constructor(args) {
-    super(args);
-    this.state = {
-      nowImage: 1,
+function getRndOne(...numbers) {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+export default function Glich() {
+  const canvasRef = useRef();
+  const { width, height } = useContext(CanvasContext);
+  const [nowImage, setNowImage] = useState(1);
+  const imageList = [TestImg, Test1Img, Test2Img];
+
+  let drawer = null;
+
+  useEffect(() => {
+    const { current } = canvasRef;
+    if (current) {
+      drawer = new V3D12_GlichImpl(current, width, height, imageList[nowImage], 30);
+    }
+    return () => {
+      drawer.reset();
+      drawer = null;
     };
-    this.width = 250;
-    this.height = 250;
-    this.imageList = [TestImg, Test1Img, Test2Img];
-    this.concussionIds = [];
-    this.animateIds = [];
-  }
+  }, [canvasRef, width, height, nowImage]);
 
-  saveRef = (canvas) => {
-    if (canvas) {
-      this.canvas = canvas;
-      this.width = canvas.width;
-      this.height = canvas.height;
-    }
-  };
-
-  componentDidMount() {
-    this.img = new Image();
-    this.img.src = Test1Img;
-    this.img.onload = () => {
-      this.reset();
-    };
-  }
-
-  onSetImage = index => {
-    this.setState({ nowImage: index });
-    this.img.src = this.imageList[index];
-    this.reset();
-  };
-
-  getRndInteger = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  getRndOne = (...numbers) => {
-    return numbers[Math.floor(Math.random() * numbers.length)];
-  };
-
-  reset = () => {
-    if (this.animateIds) {
-      this.animateIds.forEach(animateId => window.cancelAnimationFrame(animateId));
-      this.animateIds = [];
-    }
-    if (this.concussionIds.length) {
-      this.concussionIds.forEach(concussionId => window.cancelAnimationFrame(concussionId));
-      this.concussionIds = [];
-    }
-    this.pausing = false;
-    let ctx = this.canvas.getContext('2d');
-    ctx.restore();
-    ctx.clearRect(0, 0, this.width, this.height);
-    ctx.drawImage(this.img, 0, 0, this.width, this.height);
-  };
-
-  pause = () => {
-    this.pausing = !this.pausing;
-  };
-
-  random = () => {
-    this.reset();
-    let number = this.getRndInteger(1, 4);
-    let deg = this.getRndInteger(1, 179);
-    for (let i = 1; i <= number; i++) {
-      // 50 ~ 200
-      let size = this.getRndInteger(5, 20);
-      let glitch = this.getRndInteger(50 + 200 / number * (i - 1), 50 + 200 / number * i);
-      let offset = this.getRndOne(100, -100);
-      console.log(offset);
-      this.start(deg, size, offset, glitch, i);
-    }
-  };
-
-  animate = (deg, size, startValue, endValue, glitch, index, done) => {
-    const start = Date.now();
-    const duration = 30;
-    const finish = start + duration;
-    let time;
-    let byValue = endValue - startValue;
-    const tick = (ticktime) => {
-      if (!this.pausing) {
-        time = ticktime ? Date.now() : start;
-        const currentTime = time > finish ? duration : (time - start);
-        const current = startValue + byValue * currentTime / duration;
-        this.drawGlitch(deg, size, current, glitch);
-        if (time > finish) {
-          done();
-          return;
-        }
+  function random() {
+    if (drawer) {
+      drawer.reset();
+      const number = getRndInteger(4, 7);
+      const deg = getRndInteger(1, 179);
+      for (let i = 1; i <= number; i++) {
+        const size = getRndInteger(5, 20);
+        const glitch = getRndInteger(50 + 200 / number * (i - 1), 50 + 200 / number * i);
+        const offset = getRndOne(100, -100);
+        drawer.start(deg, size, offset, glitch, i);
       }
-      this.animateIds[index] = window.requestAnimFrame(tick);
-    };
-    tick();
-  };
-
-  start = (deg, size, offset, glitch, index) => {
-    let startValue = offset;
-    let endValue = 0;
-    const tick = () => {
-      if (startValue > 0) {
-        endValue = 1 - startValue;
-      } else if (startValue < 0) {
-        endValue = -1 - startValue;
-      }
-      this.animate(deg, size, startValue, endValue, glitch, index, () => {
-        this.concussionIds[index] = window.requestAnimFrame(tick);
-      });
-      if (Number(endValue.toFixed(5)) === 0) {
-        this.reset();
-        return;
-      }
-      startValue = endValue;
-    };
-    tick();
-  };
-
-  translateImage(ctx, targetX, targetY, points) {
-    ctx.save();
-    ctx.beginPath();
-    ctx.moveTo(points[0].x, points[0].y);
-    ctx.lineTo(points[1].x, points[1].y);
-    ctx.lineTo(points[2].x, points[2].y);
-    ctx.lineTo(points[3].x, points[3].y);
-    ctx.closePath();
-    ctx.clip();
-    ctx.clearRect(0, 0, this.width, this.height);
-    ctx.drawImage(this.img, targetX, targetY, this.width, this.height);
-    ctx.restore();
-  }
-
-  drawGlitch = (deg, size, offset, glitch) => {
-    if (deg < 45) {
-      this.drawXGlitch(deg, size, offset, glitch);
-    } else if (deg < 90) {
-      this.drawYGlitch(deg, size, offset, glitch);
-    } else if (deg < 135) {
-      this.drawLargeYGlitch(deg, size, offset, glitch);
-    } else {
-      this.drawLargeXGlitch(deg, size, offset, glitch);
     }
-  };
-
-  drawXGlitch(deg, height, offsetX, glitchY) {
-    let ctx = this.canvas.getContext('2d');
-    let tanDeg = Math.tan(Math.PI * deg / 180);
-    let offsetHeight = tanDeg * this.width;
-    let points = [
-      { x: 0, y: glitchY },
-      { x: this.width, y: glitchY - offsetHeight },
-      { x: this.width, y: glitchY - offsetHeight + height },
-      { x: 0, y: glitchY + height },
-    ];
-    this.translateImage(ctx, offsetX, -tanDeg * offsetX, points);
   }
 
-  drawYGlitch(deg, width, offsetY, glitchX) {
-    let ctx = this.canvas.getContext('2d');
-    let tanDeg = Math.tan(Math.PI * deg / 180);
-    let offsetWidth = this.height / tanDeg;
-    let points = [
-      { x: glitchX, y: 0 },
-      { x: glitchX - offsetWidth, y: this.height },
-      { x: glitchX - offsetWidth + width, y: this.height },
-      { x: glitchX + width, y: 0 },
-    ];
-    this.translateImage(ctx, -offsetY / tanDeg, offsetY, points);
+  function pause() {
+    if (drawer) {
+      drawer.pause();
+    }
   }
 
-  drawLargeYGlitch(deg, width, offsetY, glitchX) {
-    let ctx = this.canvas.getContext('2d');
-    let tanDeg = Math.tan(Math.PI * (180 - deg) / 180);
-    let offsetWidth = this.height / tanDeg;
-    let points = [
-      { x: glitchX, y: 0 },
-      { x: glitchX + offsetWidth, y: this.height },
-      { x: glitchX + offsetWidth + width, y: this.height },
-      { x: glitchX + width, y: 0 },
-    ];
-    this.translateImage(ctx, offsetY / tanDeg, offsetY, points);
+  function reset() {
+    if (drawer) {
+      drawer.reset();
+    }
   }
 
-  drawLargeXGlitch(deg, height, offsetX, glitchY) {
-    let ctx = this.canvas.getContext('2d');
-    let tanDeg = Math.tan(Math.PI * (180 - deg) / 180);
-    let offsetHeight = tanDeg * this.width;
-    let points = [
-      { x: 0, y: glitchY },
-      { x: this.width, y: glitchY + offsetHeight },
-      { x: this.width, y: glitchY + offsetHeight + height },
-      { x: 0, y: glitchY + height },
-    ];
-    this.translateImage(ctx, offsetX, tanDeg * offsetX, points);
-  }
-
-  render() {
-    const { nowImage } = this.state;
-    return (
-      <div className='filter'>
-        <div className='filter-t-shirt'>
-          <canvas
-            width={this.width}
-            height={this.height}
-            className='filter-canvas'
-            ref={this.saveRef}
-          />
-        </div>
-        <div className='operation-area'>
-          <Button onClick={this.random} type='primary'>随机割裂</Button>
-          &nbsp;&nbsp;
-          <Button onClick={this.pause} type='primary'>暂停</Button>
-          &nbsp;&nbsp;
-          <Button onClick={this.reset}>重置</Button>
-          <br />
-          <div className='image-list'>
-            <div className='image-list-label'>选择图片：</div>
-            {this.imageList.map((image, index) => (
+  return (
+    <div className='filter'>
+      <div className='filter-t-shirt'>
+        <canvas
+          width={width}
+          height={height}
+          className='filter-canvas'
+          ref={canvasRef}
+        />
+      </div>
+      <div className='operation-area'>
+        <Button onClick={random} type='primary'>随机割裂</Button>
+        &nbsp;&nbsp;
+        <Button onClick={pause} type='primary'>暂停</Button>
+        &nbsp;&nbsp;
+        <Button onClick={reset}>重置</Button>
+        <br />
+        <div className='image-list'>
+          <div className='image-list-label'>选择图片：</div>
+          {
+            imageList.map((image, index) => (
               <img
                 key={index}
                 className={nowImage === index ? 'selected' : ''}
                 src={image}
-                onClick={() => this.onSetImage(index)}
+                onClick={() => setNowImage(index)}
                 alt=""
               />
-            ))}
-          </div>
+            ))
+          }
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default WrappedFilter;
---
+++ src/v3d/V3D12_GlichImpl.js
@@ -0,0 +1,155 @@
+export default class V3D12_GlichImpl {
+
+  constructor(canvas, width, height, img, duration) {
+    this.ctx = canvas.getContext('2d');
+    this.width = width;
+    this.height = height;
+    this.duration = duration;
+    this.img = new Image();
+    this.img.src = img;
+    this.img.onload = () => {
+      this.reset();
+    };
+    this.concussionIds = [];
+    this.animateIds = [];
+  }
+
+  reset() {
+    const { animateIds, concussionIds, ctx } = this;
+    if (animateIds.length) {
+      animateIds.forEach(animateId => window.cancelAnimationFrame(animateId));
+      this.animateIds = [];
+    }
+    if (concussionIds.length) {
+      concussionIds.forEach(concussionId => window.cancelAnimationFrame(concussionId));
+      this.concussionIds = [];
+    }
+    this.pausing = false;
+    ctx.restore();
+    ctx.clearRect(0, 0, this.width, this.height);
+    ctx.drawImage(this.img, 0, 0, this.width, this.height);
+  }
+
+  pause() {
+    this.pausing = !this.pausing;
+  }
+
+  animate(deg, size, startValue, endValue, glitch, index, done) {
+    const { duration, animateIds } = this;
+    const start = Date.now();
+    const finish = start + duration;
+    let time;
+    let byValue = endValue - startValue;
+    const tick = (ticktime) => {
+      if (!this.pausing) {
+        time = ticktime ? Date.now() : start;
+        const currentTime = time > finish ? duration : (time - start);
+        const current = startValue + byValue * currentTime / duration;
+        this.drawGlitch(deg, size, current, glitch);
+        if (time > finish) {
+          done();
+          return;
+        }
+      }
+      animateIds[index] = window.requestAnimFrame(tick);
+    };
+    tick();
+  }
+
+  start = (deg, size, offset, glitch, index) => {
+    let startValue = offset;
+    let endValue = 0;
+    const tick = () => {
+      if (startValue > 0) {
+        endValue = 1 - startValue;
+      } else if (startValue < 0) {
+        endValue = -1 - startValue;
+      }
+      this.animate(deg, size, startValue, endValue, glitch, index, () => {
+        this.concussionIds[index] = window.requestAnimFrame(tick);
+      });
+      if (Number(endValue.toFixed(5)) === 0) {
+        this.reset();
+        return;
+      }
+      startValue = endValue;
+    };
+    tick();
+  };
+
+  translateImage(targetX, targetY, points) {
+    const { width, height, img, ctx } = this;
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(points[0].x, points[0].y);
+    ctx.lineTo(points[1].x, points[1].y);
+    ctx.lineTo(points[2].x, points[2].y);
+    ctx.lineTo(points[3].x, points[3].y);
+    ctx.closePath();
+    ctx.clip();
+    ctx.clearRect(0, 0, width, height);
+    ctx.drawImage(img, targetX, targetY, width, height);
+    ctx.restore();
+  }
+
+  drawGlitch(deg, size, offset, glitch) {
+    if (deg < 45) {
+      this.drawXGlitch(deg, size, offset, glitch);
+    } else if (deg < 90) {
+      this.drawYGlitch(deg, size, offset, glitch);
+    } else if (deg < 135) {
+      this.drawLargeYGlitch(deg, size, offset, glitch);
+    } else {
+      this.drawLargeXGlitch(deg, size, offset, glitch);
+    }
+  }
+
+  drawXGlitch(deg, height, offsetX, glitchY) {
+    let tanDeg = Math.tan(Math.PI * deg / 180);
+    let offsetHeight = tanDeg * this.width;
+    let points = [
+      { x: 0, y: glitchY },
+      { x: this.width, y: glitchY - offsetHeight },
+      { x: this.width, y: glitchY - offsetHeight + height },
+      { x: 0, y: glitchY + height },
+    ];
+    this.translateImage(offsetX, -tanDeg * offsetX, points);
+  }
+
+  drawYGlitch(deg, width, offsetY, glitchX) {
+    let tanDeg = Math.tan(Math.PI * deg / 180);
+    let offsetWidth = this.height / tanDeg;
+    let points = [
+      { x: glitchX, y: 0 },
+      { x: glitchX - offsetWidth, y: this.height },
+      { x: glitchX - offsetWidth + width, y: this.height },
+      { x: glitchX + width, y: 0 },
+    ];
+    this.translateImage(-offsetY / tanDeg, offsetY, points);
+  }
+
+  drawLargeYGlitch(deg, width, offsetY, glitchX) {
+    let tanDeg = Math.tan(Math.PI * (180 - deg) / 180);
+    let offsetWidth = this.height / tanDeg;
+    let points = [
+      { x: glitchX, y: 0 },
+      { x: glitchX + offsetWidth, y: this.height },
+      { x: glitchX + offsetWidth + width, y: this.height },
+      { x: glitchX + width, y: 0 },
+    ];
+    this.translateImage(offsetY / tanDeg, offsetY, points);
+  }
+
+  drawLargeXGlitch(deg, height, offsetX, glitchY) {
+    let tanDeg = Math.tan(Math.PI * (180 - deg) / 180);
+    let offsetHeight = tanDeg * this.width;
+    let points = [
+      { x: 0, y: glitchY },
+      { x: this.width, y: glitchY + offsetHeight },
+      { x: this.width, y: glitchY + offsetHeight + height },
+      { x: 0, y: glitchY + height },
+    ];
+    this.translateImage(offsetX, tanDeg * offsetX, points);
+  }
+
+}
